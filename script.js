9705bcccd7e332fd946a92fdd81a66fb6b9bf42a
unholygod96 <23f3000516@ds.study.iitm.ac.in>
2025-01-18 09:59:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,17 @@ document.addEventListener("DOMContentLoaded", function () {
     }
     startCountdown();
 
-    // Toggle Music
+    // Toggle Love Letter Visibility
+    document.getElementById("love-letter-button").addEventListener("click", function() {
+        document.getElementById("love-letter").classList.toggle("hidden");
+    });
+
+    // Toggle Menu Visibility
+    document.getElementById("menu-button").addEventListener("click", function () {
+        document.getElementById("side-menu").classList.toggle("hidden");
+    });
+
+    // Toggle Background Music
     document.getElementById("toggle-music").addEventListener("click", function () {
         let music = document.getElementById("background-music");
         if (music.paused) {
