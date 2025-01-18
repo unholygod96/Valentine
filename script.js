476d411b35061e1a7160ec292e0c6c4d6ccd7eac
unholygod96 <23f3000516@ds.study.iitm.ac.in>
2025-01-18 10:37:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -27,24 +27,4 @@ document.addEventListener("DOMContentLoaded", function () {
             startCountdown(newDate);
         }
     });
-
-    // Toggle Love Letter Visibility
-    document.getElementById("love-letter-button").addEventListener("click", function() {
-        document.getElementById("love-letter").classList.toggle("hidden");
-    });
-
-    // Toggle Menu Visibility
-    document.getElementById("menu-button").addEventListener("click", function () {
-        document.getElementById("side-menu").classList.toggle("hidden");
-    });
-
-    // Toggle Background Music
-    document.getElementById("toggle-music").addEventListener("click", function () {
-        let music = document.getElementById("background-music");
-        if (music.paused) {
-            music.play();
-        } else {
-            music.pause();
-        }
-    });
 });
