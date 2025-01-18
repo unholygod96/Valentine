9baaedae48297d82065b0012610d83ac414a09ef
unholygod96 <23f3000516@ds.study.iitm.ac.in>
2025-01-18 10:56:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,10 +6,6 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById(tabId).style.display = 'block';
     }
 
-    document.getElementById("love-letter-button").addEventListener("click", function() {
-        document.getElementById("love-letter").classList.toggle("hidden");
-    });
-
     // Toggle menu visibility
     document.getElementById("menu-button").addEventListener("click", function() {
         let menu = document.getElementById("side-menu");
@@ -17,6 +13,16 @@ document.addEventListener("DOMContentLoaded", function () {
             menu.style.display = "block";
         } else {
             menu.style.display = "none";
+        }
+    });
+
+    // Toggle love letter visibility
+    document.getElementById("love-letter-button").addEventListener("click", function() {
+        let loveLetter = document.getElementById("love-letter");
+        if (loveLetter.style.display === "none" || loveLetter.style.display === "") {
+            loveLetter.style.display = "block";
+        } else {
+            loveLetter.style.display = "none";
         }
     });
 });
